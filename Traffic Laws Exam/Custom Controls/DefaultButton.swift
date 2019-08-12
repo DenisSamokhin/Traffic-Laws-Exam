a8e935a8cb6daac0e616dcd0063f7c9bde78c344
Denis Samokhin <denis.samokhin2011@gmail.com>
2019-08-12 20:18:44 +0300

    Add default button class and Color scheme struct

---
+++ Traffic Laws Exam/Custom Controls/DefaultButton.swift
@@ -0,0 +1,31 @@
+//
+//  DefaultButton.swift
+//  Traffic Laws Exam
+//
+//  Created by Denis on 8/12/19.
+//  Copyright © 2019 ITLions. All rights reserved.
+//
+
+import UIKit
+
+class DefaultButton: UIButton {
+    
+    var cornerRadius: CGFloat
+
+    init(_ cornerRadius: CGFloat = 5.0, title: String) {
+        self.cornerRadius = cornerRadius
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        setupDefaultUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupDefaultUI() {
+        self.backgroundColor = ButtonSettings.Colors.DefaultBgColor
+        self.setTitleColor(ButtonSettings.Colors.DefaultTextColor, for: .normal)
+        self.layer.cornerRadius = cornerRadius
+    }
+}
---
+++ Traffic Laws Exam/Utils/Utils.swift
@@ -0,0 +1,17 @@
+//
+//  Utils.swift
+//  Traffic Laws Exam
+//
+//  Created by Denis on 8/12/19.
+//  Copyright © 2019 ITLions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ButtonSettings {
+    struct Colors {
+        static var DefaultBgColor = UIColor.white
+        static var DefaultTextColor = UIColor.black
+    }
+}
