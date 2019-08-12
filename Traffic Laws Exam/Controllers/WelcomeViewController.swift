7edfdd56d6d62a375514c5551a1a057925e6d6b7
Denis Samokhin <denis.samokhin2011@gmail.com>
2019-08-12 20:19:03 +0300

    Implement basic UI onWelcome screen

---
+++ Traffic Laws Exam/Controllers/WelcomeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Traffic Laws Exam
+//
+//  Created by Denis on 8/12/19.
+//  Copyright © 2019 ITLions. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    let examButton = DefaultButton(title: "Начать тест")
+    let profileButton = DefaultButton(title: "Мои результаты")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.orange
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        setUI()
+    }
+    
+    // MARK: UI
+    
+    func setUI() {
+        setupExamButton()
+        setupProfileButton()
+    }
+    
+    func setupExamButton() {
+        examButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(examButton)
+        
+        examButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        examButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -40).isActive = true
+        examButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        examButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+    }
+    
+    func setupProfileButton() {
+        profileButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(profileButton)
+        
+        profileButton.centerXAnchor.constraint(equalTo: examButton.centerXAnchor).isActive = true
+        profileButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 40).isActive = true
+        profileButton.widthAnchor.constraint(equalTo: examButton.widthAnchor).isActive = true
+        profileButton.heightAnchor.constraint(equalTo: examButton.heightAnchor).isActive = true
+    }
+
+
+}
+
