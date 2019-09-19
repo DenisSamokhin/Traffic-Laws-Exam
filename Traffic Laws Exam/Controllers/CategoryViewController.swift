8a8a3ef1f2e53559338fbcbeac5215790b93b763
Denis Samokhin <denis.samokhin2011@gmail.com>
2019-09-19 15:11:52 +0300

    Make Collection view layout flexible on Categories screen

--- Traffic Laws Exam/Controllers/CategoryViewController.swift
+++ Traffic Laws Exam/Controllers/CategoryViewController.swift
@@ -72,18 +72,22 @@ class CategoryViewController: UIViewController {
         cv.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
     
+    private func twoSectionMode() -> Bool {
+        return self.viewModel.categoriesTitles.count % 2 != 0
+    }
 
 }
 
 extension CategoryViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return (section == 0) ? self.viewModel.categoriesTitles.count - 1 : 1
+        return (twoSectionMode()) ? ((section == 0) ? self.viewModel.categoriesTitles.count - 1 : 1) : self.viewModel.categoriesTitles.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? CategoryCollectionViewCell else { return UICollectionViewCell() }
-        cell.titleLabel.text = (indexPath.section == 1) ? viewModel.categoriesTitles.last : viewModel.categoriesTitles[indexPath.row]
-        if let imgName = (indexPath.section == 1) ? viewModel.categoriesImages.last : viewModel.categoriesImages[indexPath.row], imgName != "" {
+        cell.titleLabel.text = (twoSectionMode()) ? ((indexPath.section == 1) ? viewModel.categoriesTitles.last : viewModel.categoriesTitles[indexPath.row]) : viewModel.categoriesTitles[indexPath.row]
+        
+        if let imgName = (twoSectionMode()) ? ((indexPath.section == 1) ? viewModel.categoriesImages.last : viewModel.categoriesImages[indexPath.row]) : viewModel.categoriesImages[indexPath.row], imgName != "" {
             cell.imageView.image = ImageManager.shared.load(image: imgName)
         }else {
             cell.imageView.image = UIImage(named: Constants.ImageNames.categoriesPlaceholderImage)
@@ -94,12 +98,12 @@ extension CategoryViewController: UICollectionViewDataSource {
 
 extension CategoryViewController: UICollectionViewDelegate {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return (twoSectionMode()) ? 2 : 1
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCollectionViewCell else { return }
-        let categoryId = (indexPath.section == 0) ? viewModel.getID(index: indexPath.row) : 0
+        let categoryId = (twoSectionMode()) ? ((indexPath.section == 0) ? viewModel.getID(index: indexPath.row) : 0) : ((indexPath.row == viewModel.categoriesTitles.count - 1) ? 0 : viewModel.getID(index: indexPath.row))
         guard let coordinator = coordinator else { return }
         coordinator.goToExamScreen(categoryId: categoryId)
     }
@@ -110,6 +114,6 @@ extension CategoryViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let itemWidth = self.view.frame.size.width / 2
         let itemHeight = itemWidth * 0.8
-        return (indexPath.section == 1) ? CGSize(width: collectionView.frame.size.width, height: itemHeight) : CGSize(width: itemWidth, height: itemHeight)
+        return (twoSectionMode()) ? ((indexPath.section == 1) ? CGSize(width: collectionView.frame.size.width, height: itemHeight) : CGSize(width: itemWidth, height: itemHeight)) : CGSize(width: itemWidth, height: itemHeight)
     }
 }
